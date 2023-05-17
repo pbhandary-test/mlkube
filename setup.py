a39bd740984be7961793a2abb7ca601dabcdf30b
Praneeth Bhandary <pbhandary@gmail.com>
2023-05-17 08:09:43 +0000

    first commit check

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->list[str]:
+    requirements = [] 
+    with open(file_path) as f:
+        requirements = [lines.replace("\n","" ) for lines in f.readlines()]
+    
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+setup(
+name = 'kubeflow_project',
+version = '0.0.1',
+author = 'praneeth_bhandary',
+author_email = 'praneethbhandary@gmail.com',
+packages = find_packages(),
+install_requires = get_requirements('requirements.txt')
+)
